dd536dfb4687d75203e33115c6cb5380a4aebab5
EVC Chronicles Admin <tripwireplease@gmail.com>
2021-04-02 13:58:38 +0800

    update

--- hy-data-analysis-with-python-2020/part06-e04_spam_detection/src/spam_detection.py
+++ hy-data-analysis-with-python-2020/part06-e04_spam_detection/src/spam_detection.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import gzip
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 
 def spam_detection(random_state=0, fraction=1.0):
+    # read zipped data in arrays of (fraction) number of lines
     ham, spam = gzip.open('src/ham.txt.gz').read(), gzip.open('src/spam.txt.gz').read()
-    ham_data, spam_data = []
+    ham_data, spam_data = [], []
     for x in range(0, int(len(ham) * fraction)):
         ham_data.append(x)
     for x in range(0, int(len(spam)*fraction)):
         spam_data.append(x)
 
-    features = CountVectorizer.fit_transform(ham_data, spam_data)
-    X_train, X_test, y_train, y_test = train_test_split(features)
+
+    vec = CountVectorizer()
+    features = vec.fit_transform(ham_data)
+    #X_train, X_test, y_train, y_test = train_test_split(features)
     return 0.0, 0, 0
 
 def main():
